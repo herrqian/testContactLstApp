96542594fa482adb53bb07a89b204ebb84ec7a76
Jing Qian <ji431qia@htwg-konstanz.de>
2024-04-20 17:45:40 +0200

    add wait before check err message

--- utils/pages/SignUpPage.py
+++ utils/pages/SignUpPage.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+from utils.data.common_data import LOCATE_BY_XPATH, LOCATE_BY_ID
 from utils.locators.signup_locators import FIRST_NAME, LAST_NAME, EMAIL, ERROR, PASSWORD, SUBMIT_BTN
 from utils.pages.BasePage import BasePage
 
@@ -15,30 +16,31 @@ class SignUpPage(BasePage):
         self.__set_email(credentials["EMAIL"])
         self.__set_password(credentials["PASSWORD"])
         self.logger.info("Click the SUBMIT button")
-        self.driver.find_element(By.XPATH, SUBMIT_BTN).click()
+        self.find_element(self.driver, By.XPATH, SUBMIT_BTN).click()
 
     def __set_first_name(self, first_name):
-        self.driver.find_element(By.XPATH, FIRST_NAME).clear()
+        self.find_element(self.driver, LOCATE_BY_XPATH, FIRST_NAME).clear()
         self.logger.info(f"Set the first name = {first_name}")
-        self.driver.find_element(By.XPATH, FIRST_NAME).send_keys(first_name)
+        self.find_element(self.driver, LOCATE_BY_XPATH, FIRST_NAME).send_keys(first_name)
 
     def __set_last_name(self, last_name):
-        self.driver.find_element(By.XPATH, LAST_NAME).clear()
+        self.find_element(self.driver, LOCATE_BY_XPATH, LAST_NAME).clear()
         self.logger.info(f"Set the last name = {last_name}")
-        self.driver.find_element(By.XPATH, LAST_NAME).send_keys(last_name)
+        self.find_element(self.driver, LOCATE_BY_XPATH, LAST_NAME).send_keys(last_name)
 
     def __set_email(self, email):
-        self.driver.find_element(By.XPATH, EMAIL).clear()
+        self.find_element(self.driver, LOCATE_BY_XPATH, EMAIL).clear()
         self.logger.info(f"Set the email = {email}")
-        self.driver.find_element(By.XPATH, EMAIL).send_keys(email)
+        self.find_element(self.driver, LOCATE_BY_XPATH, EMAIL).send_keys(email)
 
     def __set_password(self, password):
-        self.driver.find_element(By.XPATH, PASSWORD).clear()
+        self.find_element(self.driver, LOCATE_BY_XPATH, PASSWORD).clear()
         self.logger.info(f"Set the password = {password}")
-        self.driver.find_element(By.XPATH, PASSWORD).send_keys(password)
+        self.find_element(self.driver, LOCATE_BY_XPATH, PASSWORD).send_keys(password)
 
     def is_signup_failed(self):
-        err_text = self.driver.find_element(By.ID, ERROR).text
+        self.wait_element_to_be_visible(self.driver, LOCATE_BY_ID, ERROR)
+        err_text = self.find_element(self.driver, LOCATE_BY_ID, ERROR).text
         self.logger.info(f"Error message in span = {err_text}")
         return "User validation failed" in err_text
 

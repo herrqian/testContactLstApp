33ecb12fe01505557dd373f374f273d0dcae0e6e
Jing Qian <ji431qia@htwg-konstanz.de>
2024-04-20 17:19:10 +0200

    add locators

---
+++ utils/locators/signup_locators.py
@@ -0,0 +1,8 @@
+# sign up page
+FIRST_NAME = '//*[@id="firstName"]'
+LAST_NAME = '//*[@id="lastName"]'
+EMAIL = '//*[@id="email"]'
+PASSWORD = '//*[@id="password"]'
+SUBMIT_BTN = '//*[@id="submit"]'
+CANCEL_BTN = ""
+ERROR = "error"

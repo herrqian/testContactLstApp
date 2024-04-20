5d25d6c639c4c13bfd86f4c434a88a16d57dbfce
Jing Qian <ji431qia@htwg-konstanz.de>
2024-04-20 17:19:46 +0200

    add testcases for login testsuite

--- test_suits/test_login_page/conftest.py
+++ test_suits/test_login_page/conftest.py
@@ -5,8 +5,15 @@ from selenium import webdriver
 from selenium.webdriver import Chrome, Firefox, Edge
 
 
-@pytest.fixture(scope="module")
-def setup() -> Union[Chrome, Firefox, Edge]:
+@pytest.fixture(scope="function")
+def setup_signin() -> Union[Chrome, Firefox, Edge]:
     driver = webdriver.Chrome()
     driver.get("https://thinking-tester-contact-list.herokuapp.com/login")
+    return driver
+
+
+@pytest.fixture(scope="function")
+def setup_signup() -> Union[Chrome, Firefox, Edge]:
+    driver = webdriver.Chrome()
+    driver.get("https://thinking-tester-contact-list.herokuapp.com/addUser")
     return driver
--- test_suits/test_login_page/test_login_func.py
+++ test_suits/test_login_page/test_login_func.py
@@ -1,16 +1,32 @@
 import pytest
 
 from utils.pages.LoginPage import LoginPage
+from utils.pages.SignUpPage import SignUpPage
 
 
-def test_signup_with_invalid_input(setup):
-    login_page = LoginPage(setup)
+def test_signup_with_invalid_credentials(setup_signup):
+    signup_page = SignUpPage(setup_signup)
+    credentials = {
+        "FIRST_NAME": "ADAMS",
+        "LAST_NAME": "SMITH",
+        "EMAIL": "abc@123.de",
+        "PASSWORD": "1234",
+    }
+    signup_page.signup(credentials)
+    expected_res = True
+    got_res = signup_page.is_signup_failed()
+    if expected_res == got_res:
+        signup_page.logger.info("PASSED")
+    else:
+        signup_page.logger.warn("FAILED")
 
-    login_page.signup()
+
+def test_sign_up_with_valid_credentials(setup_signup):
+    login_page = SignUpPage(setup_signup)
 
 
-def test_title(setup):
-    login_page = LoginPage(setup)
+def test_title(setup_signin):
+    login_page = LoginPage(setup_signin)
     expected_res = "Contact List App"
     got_res = login_page.title()
     login_page.logger.info(f"Expected Title of Website = {expected_res}")

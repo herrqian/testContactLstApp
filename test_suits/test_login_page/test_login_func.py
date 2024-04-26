6c8740bdc824997cb180cbe4446dedc6c1936e63
Jing Qian <ji431qia@htwg-konstanz.de>
2024-04-26 17:24:02 +0200

    refactor testcases

--- test_suits/test_login_page/test_login_func.py
+++ test_suits/test_login_page/test_login_func.py
@@ -1,33 +1,46 @@
+import time
+
 import pytest
 
+from test_suits.test_login_page.test_data import invalid_credentials, valid_credentials
 from utils.pages.LoginPage import LoginPage
 from utils.pages.SignUpPage import SignUpPage
 
 
-def test_signup_with_invalid_credentials(setup_signup):
-    signup_page = SignUpPage(setup_signup)
-    credentials = {
-        "FIRST_NAME": "ADAMS",
-        "LAST_NAME": "SMITH",
-        "EMAIL": "abc@123.de",
-        "PASSWORD": "1234",
-    }
-    signup_page.signup(credentials)
-    expected_res = True
-    got_res = signup_page.is_signup_failed()
-    assert expected_res == got_res, signup_page.logger.warn("FAILED")
-    signup_page.logger.info("PASSED")
+class TestSignUp:
+    @pytest.mark.usefixtures("setup_signup")
+    @pytest.mark.parametrize("credentials", invalid_credentials)
+    def test_signup_with_invalid_credentials(self, setup_signup, credentials):
+        signup_page = setup_signup
+        signup_page.signup(credentials)
+        expected_res = True
+        got_res = signup_page.is_signup_failed()
+        assert expected_res == got_res, signup_page.logger.warn("FAILED")
+        signup_page.logger.info("PASSED")
+        del signup_page
+
+    @pytest.mark.usefixtures("setup_signup")
+    @pytest.mark.parametrize("credentials", valid_credentials)
+    def test_sign_up_with_valid_credentials(self, setup_signup, credentials):
+        signup_page = setup_signup
+        signup_page.signup(credentials)
+        expected_res = True
+        got_res = signup_page.is_signup_succeed()
+        assert expected_res == got_res, signup_page.logger.warn("FAILED")
+        signup_page.logger.info("PASSED")
+        del signup_page
 
 
-def test_sign_up_with_valid_credentials(setup_signup):
-    login_page = SignUpPage(setup_signup)
+class TestSignIn:
+
+    @pytest.mark.usefixtures("setup_signin")
+    def test_title(self, setup_signin):
+        login_page = setup_signin
+        expected_res = "Contact List App"
+        got_res = login_page.title()
+        login_page.logger.info(f"Expected Title of Website = {expected_res}")
+        login_page.logger.info(f"Get Title of Website = {got_res}")
+        assert expected_res == got_res, login_page.logger.warn("FAILED")
+        login_page.logger.info("PASSED")
 
 
-def test_title(setup_signin):
-    login_page = LoginPage(setup_signin)
-    expected_res = "Contact List App"
-    got_res = login_page.title()
-    login_page.logger.info(f"Expected Title of Website = {expected_res}")
-    login_page.logger.info(f"Get Title of Website = {got_res}")
-    assert expected_res == got_res, login_page.logger.warn("FAILED")
-    login_page.logger.info("PASSED")

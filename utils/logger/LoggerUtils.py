6ad95e6098aa5b42c08d27b166e93f9e756f6c6a
Jing Qian <ji431qia@htwg-konstanz.de>
2024-06-19 23:35:52 +0200

    remove file streaming

--- utils/logger/LoggerUtils.py
+++ utils/logger/LoggerUtils.py
@@ -2,7 +2,6 @@ import logging
 import logging.handlers
 import os.path
 import pathlib
-from datetime import datetime
 
 
 def set_logger(name):
@@ -15,12 +14,12 @@ def set_logger(name):
     _logger.setLevel(logging.INFO)
 
     # File handler to move stdout to log file
-    log_file_name = filename_startswith + datetime.now().strftime("%y%m%d_%H%M%S") + ".log"
-    res_dir_path = create_res_dir()
-    file = logging.handlers.RotatingFileHandler(filename=os.path.join(res_dir_path, log_file_name))
-    file_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(name)s: %(message)s")
-    file.setLevel(logging.DEBUG)
-    file.setFormatter(file_format)
+    # log_file_name = filename_startswith + datetime.now().strftime("%y%m%d_%H%M%S") + ".log"
+    # res_dir_path = create_res_dir()
+    # file = logging.handlers.RotatingFileHandler(filename=os.path.join(res_dir_path, log_file_name))
+    # file_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(name)s: %(message)s")
+    # file.setLevel(logging.DEBUG)
+    # file.setFormatter(file_format)
 
     # Stream handler to print debug logs in stdout
     stream = logging.StreamHandler()
@@ -28,10 +27,10 @@ def set_logger(name):
     stream.setLevel(logging.DEBUG)
     stream.setFormatter(stream_format)
 
-    if (_logger.hasHandlers()):
-        _logger.handlers.clear()
+    # if (_logger.hasHandlers()):
+    #     _logger.handlers.clear()
     # Adding all handlers to log file
-    _logger.addHandler(file)
+    # _logger.addHandler(file)
     _logger.addHandler(stream)
     return _logger
 
